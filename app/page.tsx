42d465dc199e00a8db4fa7c13fa3ce18afc55d71
jellybottom <108094166+jellybottom@users.noreply.github.com>
2026-01-21 20:07:35 +0000

    basedfix

--- app/page.tsx
+++ app/page.tsx
@@ -149,6 +149,7 @@ export default function Home() {
               <div style={{ background: 'transparent', display: 'flex', alignItems: 'center' }}>
                 <Identity 
                   address={userAddress} 
+                  schemaId="0xf8b05c79f090979bf4a80270aba232dff11a10d9ca55c4f88de9531794c6ca19"
                   chain={base}
                   className="bg-transparent"
                 >
